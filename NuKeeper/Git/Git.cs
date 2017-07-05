41126d549c70ad1e0a4b3a0836c147f7e4a30fd9
Dan Cohen <daniel.cohen@just-eat.com>
2017-07-05 15:24:36 +0100

    add git implementation

---
+++ NuKeeper/Git/Git.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NuKeeper.Git
+{
+    class Git : IGit
+    {
+        public Task Pull(Uri pullEndpoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Checkout(string branchName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(string remoteName, string branchName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
