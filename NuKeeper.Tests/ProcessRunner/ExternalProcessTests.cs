101ae2911ed9ce9846676af6cb0af0db89d5dbb6
Anthony Steele <anthony.steele@just-eat.com>
2017-07-05 14:03:31 +0100

    Very basic test suite using Nunit run on commandline with:
    cd "\NuKeeper.Tests"
    dotnet test

---
+++ NuKeeper.Tests/ProcessRunner/ExternalProcessTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using NuKeeper.ProcessRunner;
+using NUnit.Framework;
+
+namespace NuKeeper.Tests.ProcessRunner
+{
+    [TestFixture]
+    public class ExternalProcessTests
+    {
+        [Test]
+        public async Task SuccessCase()
+        {
+            var process = new ExternalProcess();
+            var result = await process.Run("dir");
+
+            Assert.That(result.ExitCode, Is.EqualTo(0));
+            Assert.That(result.Output, Is.Not.Empty);
+            Assert.That(result.Success, Is.True);
+        }
+
+        [Test]
+        public async Task FailureCase()
+        {
+            var process = new ExternalProcess();
+            var result = await process.Run("dirt");
+
+            Assert.That(result.ExitCode, Is.Not.EqualTo(0));
+            Assert.That(result.Success, Is.False);
+        }
+    }
+}
