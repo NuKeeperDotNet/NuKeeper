6e8b907b51ab9384da1bf0828bf813b2b412b2ed
Dan Cohen <daniel.cohen@just-eat.com>
2017-07-07 17:52:22 +0100

    Add EasyConfig.Net.Core

---
+++ NuKeeper/Configuration/CommandLineArguments.cs
@@ -0,0 +1,23 @@
+using System;
+using EasyConfig.Attributes;
+
+namespace NuKeeper.Configuration
+{
+    public class CommandLineArguments
+    {
+        [CommandLine("mode")]
+        public string Mode;
+
+        [CommandLine("github_token"), Required, SensitiveInformation]
+        public string GithubToken;
+
+        [CommandLine("github_repository_uri")]
+        public Uri GithubRepositoryUri;
+
+        [CommandLine("github_organisation_name")]
+        public string GithubOrganisationName;
+
+        [CommandLine("github_api_endpoint")]
+        public Uri GithubApiEndpoint;
+    }
+}
