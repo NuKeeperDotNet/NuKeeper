e1b7c1153773a4d7726436ef387c90148a194bdc
Anthony Steele <anthony.steele@just-eat.com>
2017-07-08 16:49:20 +0100

    Updated Exclusions

--- NuKeeper/RepositoryInspection/DirectoryExclusions.cs
+++ NuKeeper/RepositoryInspection/DirectoryExclusions.cs
@@ -7,6 +7,18 @@ namespace NuKeeper.RepositoryInspection
 {
     public static class DirectoryExclusions
     {
+        public static bool PathIsExcluded(string path)
+        {
+            // subDir is a full path, check the last part
+            var dirName = new DirectoryInfo(path).Name;
+            return IsExcluded(dirName);
+        }
+
+        private static bool IsExcluded(string dirName)
+        {
+            return _excludedDirNames.Any(s => string.Equals(s, dirName, StringComparison.OrdinalIgnoreCase));
+        }
+
         private static readonly List<string> _excludedDirNames = new List<string>
         {
             ".git",
@@ -16,17 +28,5 @@ namespace NuKeeper.RepositoryInspection
             "node_modules",
             "packages"
         };
-
-        public static bool PathIsExcluded(string path)
-        {
-            // subDir is a full path, check the last part
-            var dirName = new DirectoryInfo(path).Name;
-            return IsExcluded(dirName);
-        }
-
-        public static bool IsExcluded(string dirName)
-        {
-            return _excludedDirNames.Any(s => string.Equals(s, dirName, StringComparison.OrdinalIgnoreCase));
-        }
     }
 }
