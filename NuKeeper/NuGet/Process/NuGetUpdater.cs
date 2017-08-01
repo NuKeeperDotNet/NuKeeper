0edb64adaefcd60f03b493f9a9c99f98e709f62d
Anthony Steele <anthony.steele@just-eat.com>
2017-08-01 10:25:51 +0100

    Command pattern refactor Push common code to check result into ExternalProcess

--- NuKeeper/NuGet/Process/NuGetUpdater.cs
+++ NuKeeper/NuGet/Process/NuGetUpdater.cs
@@ -20,17 +20,8 @@ namespace NuKeeper.NuGet.Process
             var dirName = currentPackage.Path.FullDirectory;
             var updateCommand = $"cd {dirName} & dotnet add package {currentPackage.Id} -v {newVersion}";
             Console.WriteLine(updateCommand);
-            await RunExternalCommand(updateCommand);
-        }
 
-        private async Task RunExternalCommand(string command)
-        {
-            var result = await _externalProcess.Run(command);
-
-            if (!result.Success)
-            {
-                throw new Exception($"Exit code: {result.ExitCode}\n\n{result.Output}\n\n{result.ErrorOutput}");
-            }
+            await _externalProcess.Run(updateCommand, true);
         }
     }
 }
