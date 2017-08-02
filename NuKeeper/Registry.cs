de0028389a38fa3436a3ac6dd527697714a57c2f
Anthony Steele <anthony.steele@just-eat.com>
2017-08-02 18:46:09 +0100

    refactoring the program Extracted Registry and Runner from the program The runner is the async part, and all it's args can be found by using the container :)

---
+++ NuKeeper/Registry.cs
@@ -0,0 +1,26 @@
+﻿using NuKeeper.Configuration;
+using NuKeeper.Engine;
+using NuKeeper.Github;
+using NuKeeper.NuGet.Api;
+using SimpleInjector;
+
+namespace NuKeeper
+{
+    public static class Registry
+    {
+        public static Container RegisterContainer(Settings settings)
+        {
+            var container = new Container();
+
+            container.Register(() => settings, Lifestyle.Singleton);
+            container.Register<IGithub, OctokitClient>();
+            container.Register<IGithubRepositoryDiscovery, GithubRepositoryDiscovery>();
+            container.Register<IPackageUpdateSelection, PackageUpdateSelection>();
+            container.Register<IPackageUpdatesLookup, PackageUpdatesLookup>();
+            container.Register<IBulkPackageLookup, BulkPackageLookup>();
+            container.Register<IApiPackageLookup, ApiPackageLookup>();
+
+            return container;
+        }
+    }
+}
---
+++ NuKeeper/Runner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using NuKeeper.Configuration;
+using NuKeeper.Engine;
+using NuKeeper.Git;
+using NuKeeper.Github;
+using NuKeeper.NuGet.Api;
+
+namespace NuKeeper
+{
+    public class Runner
+    {
+        private readonly IGithubRepositoryDiscovery _repositoryDiscovery;
+        private readonly IPackageUpdatesLookup _updatesLookup;
+        private readonly IPackageUpdateSelection _updateSelection;
+        private readonly IGithub _github;
+        private readonly string _githubToken;
+
+        public Runner(
+            IGithubRepositoryDiscovery repositoryDiscovery, 
+            IPackageUpdatesLookup updatesLookup, 
+            IPackageUpdateSelection updateSelection, 
+            IGithub github,
+            Settings settings)
+        {
+            _repositoryDiscovery = repositoryDiscovery;
+            _updatesLookup = updatesLookup;
+            _updateSelection = updateSelection;
+            _github = github;
+            _githubToken = settings.GithubToken;
+        }
+
+        public async Task Run(string tempDir)
+        {
+            var githubUser = await _github.GetCurrentUser();
+            Console.WriteLine($"Read github user '{githubUser}'");
+
+            var git = new LibGit2SharpDriver(tempDir, githubUser, _githubToken);
+
+            var repositories = await _repositoryDiscovery.GetRepositories();
+
+            foreach (var repository in repositories)
+            {
+                try
+                {
+                    var repositoryUpdater = new RepositoryUpdater(_updatesLookup, _github, git, tempDir, _updateSelection, repository);
+                    await repositoryUpdater.Run();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Repo failed {e.GetType().Name}: {e.Message}");
+                }
+            }
+        }
+    }
+}
