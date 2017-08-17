5e40457d13193428e1f76db5ce53f67bfa5d1f66
Anthony Steele <anthony.steele@just-eat.com>
2017-08-17 09:06:30 +0100

    Test that the container can resolve Test that the container can resolve the engine root object Having this test would have saved some time recently

---
+++ NuKeeper.Tests/ContainerRegistrationTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NuKeeper.Configuration;
+using NuKeeper.Engine;
+using NUnit.Framework;
+
+namespace NuKeeper.Tests
+{
+    [TestFixture]
+    public class ContainerRegistrationTests
+    {
+        [Test]
+        public void RootCanBeResolved()
+        {
+            var container = ContainerRegistration.Init(MakeValidSettings());
+
+            var engine = container.GetInstance<GithubEngine>();
+
+            Assert.That(engine, Is.Not.Null);
+        }
+
+        private static Settings MakeValidSettings()
+        {
+            var org = new OrganisationModeSettings
+            {
+                GithubApiBase = new Uri("https://github.com/NuKeeperDotNet"),
+                GithubToken = "abc123"
+            };
+            return new Settings(org);
+        }
+    }
+}
