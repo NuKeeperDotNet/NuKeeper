c17d2ada9d0e190b750931209ab4bede71d579ea
Anthony Steele <anthony.steele@just-eat.com>
2017-09-03 16:48:00 +0100

    Tests, tada using NSubstitute

---
+++ NuKeeper.Tests/Engine/SolutionsRestoreTests.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using NSubstitute;
+using NuKeeper.Engine;
+using NuKeeper.Files;
+using NuKeeper.NuGet.Process;
+using NUnit.Framework;
+
+namespace NuKeeper.Tests.Engine
+{
+    [TestFixture]
+    public class SolutionsRestoreTests
+    {
+        [Test]
+        public async Task WhenThereAreNoSolutionsTheCommandIsNotCalled()
+        {
+            var cmd = Substitute.For<IFileRestoreCommand>();
+            var folder = Substitute.For<IFolder>();
+
+            var solutionResture = new SolutionsRestore(cmd);
+
+            await solutionResture.Restore(folder);
+
+            await cmd.DidNotReceiveWithAnyArgs().Invoke(Arg.Any<FileInfo>());
+        }
+
+        [Test]
+        public async Task WhenThereIsOneSolutionsTheCommandIsCalled()
+        {
+            var sln = new FileInfo("foo.sln");
+
+            var cmd = Substitute.For<IFileRestoreCommand>();
+            var folder = Substitute.For<IFolder>();
+            folder.Find(Arg.Any<string>()).Returns(new[] { sln });
+
+            var solutionResture = new SolutionsRestore(cmd);
+
+            await solutionResture.Restore(folder);
+
+            await cmd.Received(1).Invoke(Arg.Any<FileInfo>());
+            await cmd.Received().Invoke(sln);
+        }
+
+        [Test]
+        public async Task WhenThereAreTwoSolutionsTheCommandIsCalledForEachOfThem()
+        {
+            var sln1 = new FileInfo("foo.sln");
+            var sln2 = new FileInfo("bar.sln");
+
+            var cmd = Substitute.For<IFileRestoreCommand>();
+            var folder = Substitute.For<IFolder>();
+            folder.Find(Arg.Any<string>()).Returns(new[] { sln1, sln2 });
+
+            var solutionResture = new SolutionsRestore(cmd);
+
+            await solutionResture.Restore(folder);
+
+            await cmd.Received(2).Invoke(Arg.Any<FileInfo>());
+            await cmd.Received().Invoke(sln1);
+            await cmd.Received().Invoke(sln2);
+        }
+    }
+}
