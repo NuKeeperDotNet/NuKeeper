ed13610157d64eefba655eb01db82f5b1d40662e
Anthony Steele <anthony.steele@just-eat.com>
2017-09-06 10:31:17 +0100

    It works! Remove the "read-only" flag before deleting as is foretold in https://stackoverflow.com/questions/1157246/unauthorizedaccessexception-trying-to-delete-a-file-in-a-folder-where-i-can-dele

--- NuKeeper/Files/Folder.cs
+++ NuKeeper/Files/Folder.cs
@@ -29,12 +29,39 @@ namespace NuKeeper.Files
 
             try
             {
-                _root.Delete(true);
+                DeleteDirectoryInternal(_root.FullName);
+                _logger.Verbose($"Deleted folder {_root.FullName}");
             }
             catch (Exception ex)
             {
                 _logger.Verbose($"Folder delete failed: {ex.GetType().Name} {ex.Message}");
             }
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/1157246/unauthorizedaccessexception-trying-to-delete-a-file-in-a-folder-where-i-can-dele
+        /// </summary>
+        /// <param name="targetDir"></param>
+        private void DeleteDirectoryInternal(string targetDir)
+        {
+            // remove any "read-only" flag that would prevent the delete
+            File.SetAttributes(targetDir, FileAttributes.Normal);
+
+            var files = Directory.GetFiles(targetDir);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            var subDirs = Directory.GetDirectories(targetDir);
+            foreach (string dir in subDirs)
+            {
+                DeleteDirectoryInternal(dir);
+            }
+
+            Directory.Delete(targetDir, false);
         }
     }
 }
