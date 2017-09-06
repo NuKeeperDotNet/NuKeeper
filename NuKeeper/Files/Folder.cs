14ce7b6314bfc98c2b14e6397cca44fec0059cdc
Anthony Steele <AnthonySteele@users.noreply.github.com>
2017-09-06 12:34:17 +0100

    Folder Delete works (#137)
    * Simpler delete code
    
    * It works!
    Remove the "read-only" flag before deleting
    as is foretold in https://stackoverflow.com/questions/1157246/unauthorizedaccessexception-trying-to-delete-a-file-in-a-folder-where-i-can-dele

--- NuKeeper/Files/Folder.cs
+++ NuKeeper/Files/Folder.cs
@@ -25,16 +25,43 @@ namespace NuKeeper.Files
 
         public void TryDelete()
         {
-            _logger.Verbose($"Attempting delete of working folder {_root.FullName}");
+            _logger.Verbose($"Attempting delete of folder {_root.FullName}");
 
             try
             {
-                _root.Delete(true);
+                DeleteDirectoryInternal(_root.FullName);
+                _logger.Verbose($"Deleted folder {_root.FullName}");
             }
             catch (Exception ex)
             {
-                _logger.Verbose($"Folder delete failed: {ex.Message}");
+                _logger.Verbose($"Folder delete failed: {ex.GetType().Name} {ex.Message}");
             }
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/1157246/unauthorizedaccessexception-trying-to-delete-a-file-in-a-folder-where-i-can-dele
+        /// </summary>
+        /// <param name="targetDir"></param>
+        private void DeleteDirectoryInternal(string targetDir)
+        {
+            // remove any "read-only" flag that would prevent the delete
+            File.SetAttributes(targetDir, FileAttributes.Normal);
+
+            var files = Directory.GetFiles(targetDir);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            var subDirs = Directory.GetDirectories(targetDir);
+            foreach (string dir in subDirs)
+            {
+                DeleteDirectoryInternal(dir);
+            }
+
+            Directory.Delete(targetDir, false);
         }
     }
 }
