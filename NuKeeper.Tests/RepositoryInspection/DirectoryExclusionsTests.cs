0a1f1c39095d6aca958897001bf8b54e61f2cf75
Anthony Steele <anthony.steele@just-eat.com>
2017-09-26 13:58:54 +0100

    tests also need to be platform-independant

--- NuKeeper.Tests/RepositoryInspection/DirectoryExclusionsTests.cs
+++ NuKeeper.Tests/RepositoryInspection/DirectoryExclusionsTests.cs
@@ -1,4 +1,5 @@
-﻿using NuKeeper.RepositoryInspection;
+﻿using System.IO;
+using NuKeeper.RepositoryInspection;
 using NUnit.Framework;
 
 namespace NuKeeper.Tests.RepositoryInspection
@@ -6,23 +7,26 @@ namespace NuKeeper.Tests.RepositoryInspection
     [TestFixture]
     public class DirectoryExclusionsTests
     {
-        [TestCase("C:\\Code\\NuKeeper\\NuKeeper\\NuKeeper.csproj")]
-        [TestCase("C:\\Code\\NuKeeper\\NuKeeper\\GitHub\\IGithub.cs")]
-        [TestCase("C:\\Code\\NuKeeper\\NuKeeper\\dustbin.cs")]
-        [TestCase("C:\\Code\\NuKeeper\\NuKeeper\\objections.cs")]
-        [TestCase("C:\\Code\\NuKeeper\\NuKeeper\\Packages.cs")]
-        public void ShouldBeIncluded(string path)
+        [TestCase("C:{sep}Code{sep}NuKeeper{sep}NuKeeper{sep}NuKeeper.csproj")]
+        [TestCase("C:{sep}Code{sep}NuKeeper{sep}NuKeeper{sep}GitHub{sep}IGithub.cs")]
+        [TestCase("C:{sep}Code{sep}NuKeeper{sep}NuKeeper{sep}dustbin.cs")]
+        [TestCase("C:{sep}Code{sep}NuKeeper{sep}NuKeeper{sep}objections.cs")]
+        [TestCase("C:{sep}Code{sep}NuKeeper{sep}NuKeeper{sep}Packages.cs")]
+        public void ShouldBeIncluded(string pathTemplate)
         {
+            var path = pathTemplate.Replace("{sep}", $"{Path.DirectorySeparatorChar}");
             var actual = DirectoryExclusions.PathIsExcluded(path);
             Assert.That(actual, Is.False);
         }
 
-        [TestCase("C:\\Code\\NuKeeper\\NuKeeper\\bin\\debug\\net461\\config.json")]
-        [TestCase("C:\\Code\\NuKeeper\\NuKeeper\\obj\\debug\\net461\\config.json")]
-        [TestCase("C:\\Code\\NuKeeper\\NuKeeper\\packages\\foo.text")]
-        [TestCase("C:\\Code\\NuKeeper\\.git\\config")]
-        public void ShouldBeExcluded(string path)
+        [TestCase("C:{sep}Code{sep}NuKeeper{sep}NuKeeper{sep}bin{sep}debug{sep}net461{sep}config.json")]
+        [TestCase("C:{sep}Code{sep}NuKeeper{sep}NuKeeper{sep}obj{sep}debug{sep}net461{sep}config.json")]
+        [TestCase("C:{sep}Code{sep}NuKeeper{sep}NuKeeper{sep}packages{sep}foo.text")]
+        [TestCase("C:{sep}Code{sep}NuKeeper{sep}.git{sep}config")]
+        public void ShouldBeExcluded(string pathTemplate)
         {
+            var path = pathTemplate.Replace("{sep}", $"{Path.DirectorySeparatorChar}");
+
             var actual = DirectoryExclusions.PathIsExcluded(path);
             Assert.That(actual, Is.True);
         }
