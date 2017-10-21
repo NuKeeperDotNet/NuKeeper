e6e4ca7ef9e23ca5a739d84d175a8bcd0176900b
Anthony Steele <anthony.steele@just-eat.com>
2017-10-21 17:38:37 +0100

    Some happy path tests

--- NuKeeper.Tests/Configuration/SettingsParserTests.cs
+++ NuKeeper.Tests/Configuration/SettingsParserTests.cs
@@ -1,4 +1,7 @@
-﻿using NuKeeper.Configuration;
+﻿using System;
+using NuKeeper.Configuration;
+using NuKeeper.Logging;
+using NuKeeper.NuGet.Api;
 using NUnit.Framework;
 
 namespace NuKeeper.Tests.Configuration
@@ -13,6 +16,58 @@ namespace NuKeeper.Tests.Configuration
             var settings = SettingsParser.ParseToSettings(raw);
 
             Assert.That(settings, Is.Null);
+        }
+
+        [Test]
+        public void ValidRepoConfigIsParsed()
+        {
+            var raw = ValidRepoSettings();
+
+            var settings = SettingsParser.ParseToSettings(raw);
+
+            Assert.That(settings, Is.Not.Null);
+            Assert.That(settings.Mode, Is.EqualTo("repository"));
+            Assert.That(settings.AllowedChange, Is.EqualTo(VersionChange.Major));
+            Assert.That(settings.LogLevel, Is.EqualTo(LogLevel.Info));
+        }
+
+        [Test]
+        public void ValidOrgConfigIsParsed()
+        {
+            var raw = ValidOrgSettings();
+
+            var settings = SettingsParser.ParseToSettings(raw);
+
+            Assert.That(settings, Is.Not.Null);
+            Assert.That(settings.Mode, Is.EqualTo("organisation"));
+            Assert.That(settings.AllowedChange, Is.EqualTo(VersionChange.Major));
+            Assert.That(settings.LogLevel, Is.EqualTo(LogLevel.Info));
+        }
+
+        private static RawConfiguration ValidRepoSettings()
+        {
+            return new RawConfiguration
+            {
+                GithubApiEndpoint = new Uri("https://api.github.com"),
+                NuGetSources = "https://api.nuget.org/v3/index.json",
+                Mode = "repository",
+                GithubRepositoryUri = new Uri("https://github.com/NuKeeperDotNet/NuKeeper"),
+                AllowedChange = "Major",
+                LogLevel = "Info"
+            };
+        }
+
+        private static RawConfiguration ValidOrgSettings()
+        {
+            return new RawConfiguration
+            {
+                GithubApiEndpoint = new Uri("https://api.github.com"),
+                NuGetSources = "https://api.nuget.org/v3/index.json",
+                Mode = "organisation",
+                GithubOrganisationName = "NuKeeperDotNet",
+                AllowedChange = "Major",
+                LogLevel = "Info"
+            };
         }
     }
 }
