a3abaf6fb23929d0085e27c65f3ba0b60b4b8e15
Anthony Steele <anthony.steele@just-eat.com>
2017-10-21 16:10:01 +0100

    A single boring test

---
+++ NuKeeper.Tests/Configuration/SettingsParserTests.cs
@@ -0,0 +1,18 @@
+﻿using NuKeeper.Configuration;
+using NUnit.Framework;
+
+namespace NuKeeper.Tests.Configuration
+{
+    [TestFixture]
+    public class SettingsParserTests
+    {
+        [Test]
+        public void UnPopulatedConfigIsNotValid()
+        {
+            var raw = new RawConfiguration();
+            var settings = SettingsParser.ParseToSettings(raw);
+
+            Assert.That(settings, Is.Null);
+        }
+    }
+}
