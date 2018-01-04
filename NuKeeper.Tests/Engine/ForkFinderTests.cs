08d6ae0c4dce581e89ed27015c13b6b12ce1145f
Anthony Steele <anthony.steele@just-eat.com>
2018-01-04 18:00:14 +0000

    Made a start on ForkFinderTests

---
+++ NuKeeper.Tests/Engine/ForkFinderTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using NSubstitute;
+using NuKeeper.Engine;
+using NuKeeper.Github;
+using NUnit.Framework;
+
+namespace NuKeeper.Tests.Engine
+{
+    [TestFixture]
+    public class ForkFinderTests
+    {
+        [Test]
+        public async Task FallbackForkIsUsedByDefault()
+        {
+            var forkFinder = new ForkFinder(Substitute.For<IGithub>(), new NullNuKeeperLogger());
+
+            var fallbackFork = new ForkData(new Uri("http://someurl.com"), "testUser", "someRepo");
+
+            var fork = await forkFinder.PushFork("testUser", "someRepo", fallbackFork);
+
+            Assert.That(fork, Is.Not.Null);
+            Assert.That(fork, Is.EqualTo(fallbackFork));
+        }
+    }
+}
