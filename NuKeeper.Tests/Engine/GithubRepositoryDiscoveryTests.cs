0dba42924320c6d3d8f9e06e6eaec6b4ec1447d4
Anthony Steele <anthony.steele@just-eat.com>
2018-01-17 10:38:41 +0000

    basic GithubRepositoryDiscoveryTests

---
+++ NuKeeper.Tests/Engine/GithubRepositoryDiscoveryTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NSubstitute;
+using NuKeeper.Configuration;
+using NuKeeper.Engine;
+using NuKeeper.Github;
+using NUnit.Framework;
+
+namespace NuKeeper.Tests.Engine
+{
+    [TestFixture]
+    public class GithubRepositoryDiscoveryTests
+    {
+        [Test]
+        public async Task SuccessInRepoMode()
+        {
+            var github = Substitute.For<IGithub>();
+            var settings = new ModalSettings
+            {
+                Mode = GithubMode.Repository,
+                Repository = new RepositorySettings()
+            };
+
+            var githubRepositoryDiscovery = new GithubRepositoryDiscovery(github, settings);
+
+            var repos = await githubRepositoryDiscovery.GetRepositories();
+
+            Assert.That(repos, Is.Not.Null);
+            Assert.That(repos.Count(), Is.EqualTo(1));
+            Assert.That(repos.First(), Is.EqualTo(settings.Repository));
+        }
+
+        [Test]
+        public async Task SuccessInOrgMode()
+        {
+            var github = Substitute.For<IGithub>();
+            var settings = new ModalSettings
+                {
+                    Mode = GithubMode.Organisation,
+                    OrganisationName = "testOrg"
+                };
+
+            var githubRepositoryDiscovery = new GithubRepositoryDiscovery(github, settings);
+
+            var repos = await githubRepositoryDiscovery.GetRepositories();
+
+            Assert.That(repos, Is.Not.Null);
+            Assert.That(repos, Is.Empty);
+        }
+    }
+}
