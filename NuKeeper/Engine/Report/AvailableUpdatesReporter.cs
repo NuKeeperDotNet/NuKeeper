015cdec74d446153e4af9bce6bb7a3c3c40077a4
Anthony Steele <anthony.steele@just-eat.com>
2018-01-28 18:30:15 +0000

    The basics

--- NuKeeper/Engine/Report/AvailableUpdatesReporter.cs
+++ NuKeeper/Engine/Report/AvailableUpdatesReporter.cs
@@ -1,5 +1,6 @@
 ﻿using NuKeeper.RepositoryInspection;
 using System.Collections.Generic;
+using System.IO;
 
 namespace NuKeeper.Engine.Report
 {
@@ -7,6 +8,32 @@ namespace NuKeeper.Engine.Report
     {
         public void Report(List<PackageUpdateSet> updates)
         {
+            using (var writer = MakeOutputStream())
+            {
+                WriteHeading(writer);
+                foreach (var update in updates)
+                {
+                    WriteLine(writer, update);
+                }
+
+                writer.Close();
+            }
+        }
+
+        private void WriteHeading(StreamWriter writer)
+        {
+            writer.WriteLine("Package Id");
+        }
+
+        private void WriteLine(StreamWriter writer, PackageUpdateSet update)
+        {
+            writer.WriteLine(update.PackageId);
+        }
+
+        private StreamWriter MakeOutputStream()
+        {
+            var output = new FileStream("nukeeeper_report.csv", FileMode.OpenOrCreate);
+            return new StreamWriter(output);
         }
     }
 }
