b7b8f4c552cda3d30fa11e7b6fbcc507220c990a
Anthony Steele <anthony.steele@just-eat.com>
2018-02-16 15:53:10 +0000

    refactoring toto: VersionChanges is static, test it with static data

--- NuKeeper/NuGet/Api/ApiPackageLookup.cs
+++ NuKeeper/NuGet/Api/ApiPackageLookup.cs
@@ -1,8 +1,5 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using NuGet.Packaging.Core;
-using NuGet.Versioning;
 
 namespace NuKeeper.NuGet.Api
 {
@@ -18,25 +15,8 @@ namespace NuKeeper.NuGet.Api
         public async Task<PackageLookupResult> FindVersionUpdate(
             PackageIdentity package, VersionChange allowedChange)
         {
-            var versions = await _packageVersionsLookup.Lookup(package.Id);
-
-            var orderedCandidates = versions
-                .OrderByDescending(p => p.Identity.Version)
-                .ToList();
-
-            var major = HighestMatch(package.Version, orderedCandidates, VersionChange.Major);
-            var minor = HighestMatch(package.Version, orderedCandidates, VersionChange.Minor);
-            var patch = HighestMatch(package.Version, orderedCandidates, VersionChange.Patch);
-            return new PackageLookupResult(allowedChange, major, minor, patch);
-        }
-
-        private static PackageSearchMedatadata HighestMatch(
-            NuGetVersion current,
-            IList<PackageSearchMedatadata> orderedCandidates,
-            VersionChange allowedChange)
-        {
-            return orderedCandidates
-                .FirstOrDefault(p => VersionChangeFilter.Filter(current, p.Identity.Version, allowedChange));
+            var foundVersions = await _packageVersionsLookup.Lookup(package.Id);
+            return VersionChanges.MakeVersions(package.Version, foundVersions, allowedChange);
         }
     }
 }
