6448a60d71ce6b78794b2e2ed622833e413912d9
Anthony Steele <anthony.steele@just-eat.com>
2018-02-28 09:22:27 +0000

    Two tests and refactor to extract common

--- NuKeeper.Tests/Engine/Report/AvailableUpdatesReporterTests.cs
+++ NuKeeper.Tests/Engine/Report/AvailableUpdatesReporterTests.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using NSubstitute;
+using NuGet.Packaging.Core;
+using NuGet.Versioning;
 using NuKeeper.Engine.Report;
+using NuKeeper.NuGet.Api;
 using NuKeeper.RepositoryInspection;
 using NUnit.Framework;
 
@@ -14,6 +18,36 @@ namespace NuKeeper.Tests.Engine.Report
         [Test]
         public void NoRowsHasHeader()
         {
+            var rows = new List<PackageUpdateSet>();
+
+            var output = ReportToString(rows);
+
+            Assert.That(output, Is.Not.Null);
+            Assert.That(output, Is.Not.Empty);
+
+            var lines = output.Split(Environment.NewLine);
+            Assert.That(lines.Length, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void OneRow()
+        {
+            var rows = new List<PackageUpdateSet>
+            {
+                UpdateSetFor(MakePackageForV110())
+            };
+
+            var output = ReportToString(rows);
+
+            Assert.That(output, Is.Not.Null);
+            Assert.That(output, Is.Not.Empty);
+
+            var lines = output.Split(Environment.NewLine);
+            Assert.That(lines.Length, Is.EqualTo(2));
+        }
+
+        private static string ReportToString(List<PackageUpdateSet> rows)
+        {
             var memoryStream = new MemoryStream();
             var writer = new StreamWriter(memoryStream);
 
@@ -23,12 +57,38 @@ namespace NuKeeper.Tests.Engine.Report
 
             var reporter = new AvailableUpdatesReporter(streamSource, new NullNuKeeperLogger());
 
-            var rows = new List<PackageUpdateSet>();
             reporter.Report("test", rows);
 
-            var output = Encoding.UTF8.GetString(memoryStream.ToArray());
-            Assert.That(output, Is.Not.Null);
-            Assert.That(output, Is.Not.Empty);
+            return StreamToString(memoryStream);
+        }
+
+        private static string StreamToString(MemoryStream memoryStream)
+        {
+            var data = Encoding.UTF8.GetString(memoryStream.ToArray());
+            return data.Trim();
+        }
+
+        private static PackageUpdateSet UpdateSetFor(params PackageInProject[] packages)
+        {
+            var newPackage = NewPackageFooBar123();
+
+            var publishedDate = new DateTimeOffset(2018, 2, 19, 11, 12, 7, TimeSpan.Zero);
+            var latest = new PackageSearchMedatadata(newPackage, "someSource", publishedDate);
+
+            var updates = new PackageLookupResult(VersionChange.Major, latest, null, null);
+            return new PackageUpdateSet(updates, packages);
+        }
+
+        private static PackageInProject MakePackageForV110()
+        {
+            var path = new PackagePath("c:\\temp", "folder\\src\\project1\\packages.config",
+                PackageReferenceType.PackagesConfig);
+            return new PackageInProject("foo.bar", "1.1.0", path);
+        }
+
+        private static PackageIdentity NewPackageFooBar123()
+        {
+            return new PackageIdentity("foo.bar", new NuGetVersion("1.2.3"));
         }
     }
 }
