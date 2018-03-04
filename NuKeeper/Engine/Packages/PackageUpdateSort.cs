b997ebcbc7fd4c85f76508ba22d535891ca2827e
Anthony Steele <anthony.steele@just-eat.com>
2018-03-04 17:32:09 +0000

    Also sort by version change, Test pass

--- NuKeeper/Engine/Packages/PackageUpdateSort.cs
+++ NuKeeper/Engine/Packages/PackageUpdateSort.cs
@@ -13,11 +13,11 @@ namespace NuKeeper.Engine.Packages
             return packages.OrderByDescending(Priority);
         }
 
-        private static int Priority(PackageUpdateSet update)
+        private static long Priority(PackageUpdateSet update)
         {
 
-            const int Shift = 100;
-            var score = update.CountCurrentVersions();
+            const long Shift = 1000;
+            long score = update.CountCurrentVersions();
             score = score * Shift;
             score = score + update.CurrentPackages.Count;
             score = score * Shift;
@@ -32,6 +32,24 @@ namespace NuKeeper.Engine.Packages
 
         private static int ScoreVersionChange(NuGetVersion newVersion, NuGetVersion oldVersion)
         {
+            var majors = newVersion.Major - oldVersion.Major;
+            if (majors > 0)
+            {
+                return majors * 100;
+            }
+
+            var minors = newVersion.Minor - oldVersion.Minor;
+            if (minors > 0)
+            {
+                return minors * 10;
+            }
+
+            var patches = newVersion.Patch - oldVersion.Patch;
+            if (patches > 0)
+            {
+                return patches;
+            }
+
             return 0;
         }
     }
