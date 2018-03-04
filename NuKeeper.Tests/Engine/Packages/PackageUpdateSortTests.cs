6e69b9896c6d4b20934df08d2682900327babac7
Anthony Steele <anthony.steele@just-eat.com>
2018-03-04 16:10:51 +0000

    Basic tests

---
+++ NuKeeper.Tests/Engine/Packages/PackageUpdateSortTests.cs
@@ -0,0 +1,65 @@
+using NuGet.Packaging.Core;
+using NuGet.Versioning;
+using NuKeeper.Engine.Packages;
+using NuKeeper.NuGet.Api;
+using NuKeeper.RepositoryInspection;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NuKeeper.Tests.Engine.Packages
+{
+    [TestFixture]
+    public class PackageUpdateSortTests
+    {
+        [Test]
+        public void CanSortWhenListIsEmpty()
+        {
+            var items = new List<PackageUpdateSet>();
+
+            var output = PackageUpdateSort.Sort(items)
+                .ToList();
+
+            Assert.That(output, Is.Not.Null);
+        }
+
+        [Test]
+        public void CanSortOneItem()
+        {
+            var items = new List<PackageUpdateSet>
+            {
+                OnePackageUpdateSet()
+            };
+
+            var output = PackageUpdateSort.Sort(items)
+                .ToList();
+
+            Assert.That(output, Is.Not.Null);
+            Assert.That(output.Count, Is.EqualTo(1));
+            Assert.That(output[0], Is.EqualTo(items[0]));
+        }
+
+        private static PackageUpdateSet UpdateSetFor(PackageIdentity package, params PackageInProject[] packages)
+        {
+            var publishedDate = new DateTimeOffset(2018, 2, 19, 11, 12, 7, TimeSpan.Zero);
+            var latest = new PackageSearchMedatadata(package, "someSource", publishedDate);
+
+            var updates = new PackageLookupResult(VersionChange.Major, latest, null, null);
+            return new PackageUpdateSet(updates, packages);
+        }
+
+        private static PackageInProject MakePackageForV110(PackageIdentity package)
+        {
+            var path = new PackagePath("c:\\temp", "folder\\src\\project1\\packages.config",
+                PackageReferenceType.PackagesConfig);
+            return new PackageInProject(package.Id, package.Version.ToString(), path);
+        }
+        private static PackageUpdateSet OnePackageUpdateSet()
+        {
+            var package = new PackageIdentity("foo.bar", new NuGetVersion("1.2.3"));
+
+            return UpdateSetFor(package, MakePackageForV110(package));
+        }
+    }
+}
