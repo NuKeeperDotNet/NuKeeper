9bf8f1438be62df8d0267529671a9c6179d3db8b
Leszek Ciesielski <skolima@gmail.com>
2018-03-14 09:51:47 +0100

    Fallback to .gitconfig if GitHub profile information is missing.
    Add output explaining what is going on.

--- NuKeeper/Engine/GithubEngine.cs
+++ NuKeeper/Engine/GithubEngine.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Threading.Tasks;
 using LibGit2Sharp;
 using NuKeeper.Configuration;
 using NuKeeper.Github;
 using NuKeeper.Files;
+using NuKeeper.Logging;
+using Octokit;
 
 namespace NuKeeper.Engine
 {
@@ -13,19 +16,22 @@ namespace NuKeeper.Engine
         private readonly IGithubRepositoryEngine _repositoryEngine;
         private readonly string _githubToken;
         private readonly IFolderFactory _folderFactory;
+        private readonly INuKeeperLogger _logger;
 
         public GithubEngine(
             IGithub github,
             IGithubRepositoryDiscovery repositoryDiscovery,
             IGithubRepositoryEngine repositoryEngine,
             GithubAuthSettings settings,
-            IFolderFactory folderFactory)
+            IFolderFactory folderFactory,
+            INuKeeperLogger logger)
         {
             _github = github;
             _repositoryDiscovery = repositoryDiscovery;
             _repositoryEngine = repositoryEngine;
             _githubToken = settings.Token;
             _folderFactory = folderFactory;
+            _logger = logger;
         }
 
         public async Task Run()
@@ -38,13 +44,28 @@ namespace NuKeeper.Engine
                 Username = githubUser.Login,
                 Password = _githubToken
             };
-            var userIdentity = new Identity(githubUser.Name, githubUser.Email);
+
+            var userIdentity = GetUserIdentity(githubUser);
 
             var repositories = await _repositoryDiscovery.GetRepositories();
 
             foreach (var repository in repositories)
             {
                 await _repositoryEngine.Run(repository, gitCreds, userIdentity);
+            }
+        }
+
+        private Identity GetUserIdentity(Account githubUser)
+        {
+            try
+            {
+                return new Identity(githubUser.Name, githubUser.Email);
+            }
+            catch (ArgumentException e)
+            {
+                _logger.Error(
+                    "GitHub user name or public email missing from profile, falling back to .gitconfig", e);
+                return null;
             }
         }
     }
