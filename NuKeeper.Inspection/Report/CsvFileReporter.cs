21f56fea3d0299fa00e36f44be0c49c82c479560
Anthony Steele <anthony.steele@just-eat.com>
2018-05-07 18:37:18 +0100

    Rename the CSV reporter

--- NuKeeper.Inspection/Report/CsvFileReporter.cs
+++ NuKeeper.Inspection/Report/CsvFileReporter.cs
@@ -9,12 +9,12 @@ using NuKeeper.Inspection.RepositoryInspection;
 
 namespace NuKeeper.Inspection.Report
 {
-    public class AvailableUpdatesReporter: IAvailableUpdatesReporter
+    public class CsvFileReporter: IAvailableUpdatesReporter
     {
         private readonly IReportStreamSource _reportStreamSource;
         private readonly INuKeeperLogger _logger;
 
-        public AvailableUpdatesReporter(IReportStreamSource reportStreamSource, INuKeeperLogger logger)
+        public CsvFileReporter(IReportStreamSource reportStreamSource, INuKeeperLogger logger)
         {
             _reportStreamSource = reportStreamSource;
             _logger = logger;
