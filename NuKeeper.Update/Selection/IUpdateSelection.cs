b1b215a3f9c716404117d2a791187425b9abaaae
Anthony Steele <anthony.steele@just-eat.com>
2018-06-03 18:40:17 +0100

    Moving some code so that filtering can happen here Without dependency on git

---
+++ NuKeeper.Update/Selection/IUpdateSelection.cs
@@ -0,0 +1,14 @@
+using NuKeeper.Inspection.RepositoryInspection;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NuKeeper.Update.Selection
+{
+    public interface IUpdateSelection
+    {
+        Task<IReadOnlyCollection<PackageUpdateSet>> Filter(
+            IReadOnlyCollection<PackageUpdateSet> potentialUpdates,
+            Func<PackageUpdateSet, Task<bool>> remoteCheck);
+    }
+}
---
+++ NuKeeper.Update/Selection/UpdateSelection.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using NuKeeper.Inspection.Logging;
+using NuKeeper.Inspection.RepositoryInspection;
+using System.Threading.Tasks;
+
+namespace NuKeeper.Update.Selection
+{
+    public class UpdateSelection : IUpdateSelection
+    {
+        private readonly INuKeeperLogger _logger;
+
+        private readonly FilterSettings _settings;
+        private readonly DateTime _maxPublishedDate;
+
+        public UpdateSelection(FilterSettings settings,
+            INuKeeperLogger logger)
+        {
+            _logger = logger;
+            _settings = settings;
+            _maxPublishedDate = DateTime.UtcNow.Subtract(settings.MinimumAge);
+        }
+
+        public async Task<IReadOnlyCollection<PackageUpdateSet>> Filter(
+            IReadOnlyCollection<PackageUpdateSet> candidates,
+            Func<PackageUpdateSet, Task<bool>> remoteCheck)
+        {
+            var filtered = ApplyFilters(candidates);
+
+            filtered = await ApplyRemoteFilter(filtered, remoteCheck);
+
+            List<PackageUpdateSet> capped = filtered
+                .Take(_settings.MaxPullRequests)
+                .ToList();
+
+            LogPackageCounts(candidates.Count, filtered.Count, capped.Count);
+
+            foreach (var updateSet in capped)
+            {
+                _logger.Info($"Selected package update of {updateSet.SelectedId} to {updateSet.SelectedVersion}");
+            }
+
+            return capped;
+        }
+
+        private IReadOnlyCollection<PackageUpdateSet> ApplyFilters(
+            IReadOnlyCollection<PackageUpdateSet> all)
+        {
+            var filteredLocally = all
+                .Where(MatchesIncludeExclude)
+                .Where(MatchesMinAge)
+                .ToList();
+
+            if (filteredLocally.Count < all.Count)
+            {
+                _logger.Verbose($"Filtered by rules from {all.Count} to {filteredLocally.Count}");
+            }
+
+            return filteredLocally;
+        }
+
+        public static async Task<IReadOnlyCollection<PackageUpdateSet>> ApplyRemoteFilter(
+            IEnumerable<PackageUpdateSet> packageUpdateSets,
+            Func<PackageUpdateSet, Task<bool>> remoteCheck)
+        {
+            var results = await packageUpdateSets
+                .WhereAsync(async p => await remoteCheck(p));
+
+            return results.ToList();
+        }
+
+        private void LogPackageCounts(int candidates, int filtered, int capped)
+        {
+            var message = $"Selection of package updates: {candidates} candiates";
+            if (filtered < candidates)
+            {
+                message += $", filtered to {filtered}";
+            }
+
+            if (capped < filtered)
+            {
+                message +=  $", capped at {capped}";
+            }
+
+            _logger.Terse(message);
+        }
+
+        private bool MatchesIncludeExclude(PackageUpdateSet packageUpdateSet)
+        {
+            return 
+                MatchesInclude(_settings.Includes, packageUpdateSet)
+                && ! MatchesExclude(_settings.Excludes, packageUpdateSet);
+        }
+
+        private static bool MatchesInclude(Regex regex, PackageUpdateSet packageUpdateSet)
+        {
+            return regex == null || regex.IsMatch(packageUpdateSet.SelectedId);
+        }
+
+        private static bool MatchesExclude(Regex regex, PackageUpdateSet packageUpdateSet)
+        {
+            return regex != null && regex.IsMatch(packageUpdateSet.SelectedId);
+        }
+
+        private bool MatchesMinAge(PackageUpdateSet packageUpdateSet)
+        {
+            var published = packageUpdateSet.Selected.Published;
+            if (!published.HasValue)
+            {
+                return true;
+            }
+
+            return published.Value.UtcDateTime <= _maxPublishedDate;
+        }
+    }
+}
