e1335b0d0cf633ec7962010121fc3d313f2b9f6e
Anthony Steele <anthony.steele@just-eat.com>
2018-06-10 12:23:23 +0100

    Add new thigns to IoC reg, so now those tests pass

--- NuKeeper/ContainerUpdateRegistration.cs
+++ NuKeeper/ContainerUpdateRegistration.cs
@@ -2,6 +2,7 @@ using NuKeeper.Configuration;
 using NuKeeper.Update;
 using NuKeeper.Update.Process;
 using NuKeeper.Update.ProcessRunner;
+using NuKeeper.Update.Selection;
 using SimpleInjector;
 
 namespace NuKeeper
@@ -11,10 +12,23 @@ namespace NuKeeper
         public static void Register(Container container, SettingsContainer settings)
         {
             container.Register(() => settings.UserSettings.NuGetSources, Lifestyle.Singleton);
+            container.Register(() => MakeFilterSettings(settings.UserSettings), Lifestyle.Singleton);
 
+            container.Register<IUpdateSelection, UpdateSelection>();
             container.Register<IFileRestoreCommand, NuGetFileRestoreCommand>();
             container.Register<IExternalProcess, ExternalProcess>();
             container.Register<IUpdateRunner, UpdateRunner>();
+        }
+
+        private static FilterSettings MakeFilterSettings(UserSettings settings)
+        {
+            return new FilterSettings
+            {
+                Excludes = settings.PackageExcludes,
+                Includes = settings.PackageIncludes,
+                MaxPullRequests = settings.MaxPullRequestsPerRepository,
+                MinimumAge = settings.MinimumPackageAge
+            };
         }
     }
 }
