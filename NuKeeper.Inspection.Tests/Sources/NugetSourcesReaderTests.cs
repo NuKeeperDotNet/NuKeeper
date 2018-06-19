36a7254a5169c77041afca3aec19cf5b715f2e97
Anthony Steele <anthony.steele@just-eat.com>
2018-06-19 11:40:00 +0100

    More tests

--- NuKeeper.Inspection.Tests/Sources/NugetSourcesReaderTests.cs
+++ NuKeeper.Inspection.Tests/Sources/NugetSourcesReaderTests.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Linq;
 using NuKeeper.Inspection.Files;
 using NuKeeper.Inspection.Sources;
@@ -25,12 +26,55 @@ namespace NuKeeper.Inspection.Tests.Sources
         {
             var reader = MakeNuGetSourcesReader(null);
 
-            var ff = new FolderFactory(new NullNuKeeperLogger());
-
-            var result = reader.Read(ff.UniqueTemporaryFolder());
+            var result = reader.Read(TemporaryFolder());
 
             Assert.That(result.Items.Count, Is.EqualTo(1));
             Assert.That(result.Items.First(), Is.EqualTo(NuGetSources.GlobalFeedUrl));
+        }
+
+        private const string ConfigFileContents =
+            @"<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <packageSources>
+    <add key=""From A file"" value=""https://fromFile1.com"" />
+  </packageSources>
+</configuration>";
+
+        [Test]
+        public void ConfigFileIsUsed()
+        {
+            var reader = MakeNuGetSourcesReader(null);
+
+            var folder = TemporaryFolder();
+            var path = Path.Join(folder.FullPath, "nuget.config");
+            File.WriteAllText(path, ConfigFileContents);
+
+            var result = reader.Read(folder);
+
+            Assert.That(result.Items.Count, Is.EqualTo(1));
+            Assert.That(result.Items.First(), Is.EqualTo("https://fromFile1.com"));
+        }
+
+
+        [Test]
+        public void SettingsOverridesConfigFile()
+        {
+            var reader = MakeNuGetSourcesReader(new NuGetSources("https://fromConfigA.com"));
+
+            var folder = TemporaryFolder();
+            var path = Path.Join(folder.FullPath, "nuget.config");
+            File.WriteAllText(path, ConfigFileContents);
+
+            var result = reader.Read(folder);
+
+            Assert.That(result.Items.Count, Is.EqualTo(1));
+            Assert.That(result.Items.First(), Is.EqualTo("https://fromConfigA.com"));
+        }
+
+        private static IFolder TemporaryFolder()
+        {
+            var ff = new FolderFactory(new NullNuKeeperLogger());
+            return ff.UniqueTemporaryFolder();
         }
 
         private static INugetSourcesReader MakeNuGetSourcesReader(NuGetSources fallbackSources)
