1f502780d564d9d5b2f5b9efe7bad078b665061a
Anthony Steele <anthony.steele@just-eat.com>
2018-06-27 09:56:47 +0100

    A second test passes

--- NuKeeper.Tests/Engine/RepositoryUpdaterTests.cs
+++ NuKeeper.Tests/Engine/RepositoryUpdaterTests.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using NSubstitute;
+using NuGet.Packaging.Core;
+using NuGet.Versioning;
 using NuKeeper.Configuration;
 using NuKeeper.Engine;
 using NuKeeper.Engine.Packages;
@@ -23,19 +25,35 @@ namespace NuKeeper.Tests.Engine
         [Test]
         public async Task WhenThereAreNoUpdates_CountIsZero()
         {
-            var repoUpdater = MakeRepositoryUpdater();
+            var repoUpdater = MakeRepositoryUpdater(new List<PackageUpdateSet>());
 
             var git = Substitute.For<IGitDriver>();
-            var repo = new RepositoryData(
-                new ForkData(new Uri("http://foo.com"), "me", "test"),
-                new ForkData(new Uri("http://foo.com"), "me", "test"));
+            var repo = MakeRepositoryData();
 
             var count = await repoUpdater.Run(git, repo);
 
             Assert.That(count, Is.EqualTo(0));
         }
 
-        private static IRepositoryUpdater MakeRepositoryUpdater()
+        [Test]
+        public async Task WhenThereIsAnUpdate_CountIsOne()
+        {
+            var repoUpdater = MakeRepositoryUpdater(
+                new List<PackageUpdateSet>
+                {
+                    UpdateSet()
+                });
+
+            var git = Substitute.For<IGitDriver>();
+            var repo = MakeRepositoryData();
+
+            var count = await repoUpdater.Run(git, repo);
+
+            Assert.That(count, Is.EqualTo(1));
+        }
+
+        private static IRepositoryUpdater MakeRepositoryUpdater(
+            List<PackageUpdateSet> updates)
         {
             var sources = Substitute.For<INuGetSourcesReader>();
             var updateFinder = Substitute.For<IUpdateFinder>();
@@ -48,7 +66,17 @@ namespace NuKeeper.Tests.Engine
                     Arg.Any<IFolder>(),
                     Arg.Any<NuGetSources>(),
                     Arg.Any<VersionChange>())
-                .Returns(new List<PackageUpdateSet>());
+                .Returns(updates);
+
+            updateSelection.SelectTargets(Arg.Any<ForkData>(), Arg.Any<IReadOnlyCollection<PackageUpdateSet>>())
+                .Returns(c => c.ArgAt<IReadOnlyCollection<PackageUpdateSet>>(1));
+
+            packageUpdater.MakeUpdatePullRequest(
+                Arg.Any<IGitDriver>(),
+                Arg.Any<PackageUpdateSet>(),
+                Arg.Any<NuGetSources>(),
+                Arg.Any<RepositoryData>())
+                .Returns(true);
 
             var repoUpdater = new RepositoryUpdater(
                 sources, updateFinder, updateSelection, packageUpdater,
@@ -56,6 +84,28 @@ namespace NuKeeper.Tests.Engine
                 reporter, new UserSettings());
 
             return repoUpdater;
+        }
+
+        private static RepositoryData MakeRepositoryData()
+        {
+            return new RepositoryData(
+                new ForkData(new Uri("http://foo.com"), "me", "test"),
+                new ForkData(new Uri("http://foo.com"), "me", "test"));
+        }
+
+        private static PackageUpdateSet UpdateSet()
+        {
+            PackageIdentity fooPackage = new PackageIdentity("foo", new NuGetVersion(1,2,3));
+            var packages = new[]
+            {
+                new PackageInProject(fooPackage, new PackagePath("c:\\foo", "bar", PackageReferenceType.PackagesConfig))
+            };
+
+            var publishedDate = new DateTimeOffset(2018, 2, 19, 11, 12, 7, TimeSpan.Zero);
+            var latest = new PackageSearchMedatadata(fooPackage, NuGetSources.GlobalFeedUrl, publishedDate, null);
+
+            var updates = new PackageLookupResult(VersionChange.Major, latest, null, null);
+            return new PackageUpdateSet(updates, packages);
         }
     }
 }
