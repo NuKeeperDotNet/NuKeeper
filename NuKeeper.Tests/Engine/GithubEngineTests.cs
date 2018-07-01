27d24eccb03ad166724e086c1bf0be57c5055dab
Anthony Steele <anthony.steele@just-eat.com>
2018-07-01 17:11:00 +0100

    Github engine, tests cases for different repo counts

--- NuKeeper.Tests/Engine/GithubEngineTests.cs
+++ NuKeeper.Tests/Engine/GithubEngineTests.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using LibGit2Sharp;
 using NSubstitute;
 using NuKeeper.Configuration;
 using NuKeeper.Engine;
@@ -13,7 +15,69 @@ namespace NuKeeper.Tests.Engine
     public class GithubEngineTests
     {
         [Test]
-        public async Task SuperHappyFunPath()
+        public async Task SuccessCaseWithNoRepos()
+        {
+            var engine = MakeGithubEngine(
+                new List<RepositorySettings>());
+
+            var count = await engine.Run();
+
+            Assert.That(count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task SuccessCaseWithOneRepo()
+        {
+            var oneRepo = new List<RepositorySettings>
+            {
+                new RepositorySettings()
+            };
+            var engine = MakeGithubEngine(oneRepo);
+
+            var count = await engine.Run();
+
+            Assert.That(count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task SuccessCaseWithTwoRepos()
+        {
+            var repos = new List<RepositorySettings>
+            {
+                new RepositorySettings(),
+                new RepositorySettings()
+            };
+            var engine = MakeGithubEngine(repos);
+
+            var count = await engine.Run();
+
+            Assert.That(count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task CountIsNotIncrementedWhenRepoEngineFails()
+        {
+            var repos = new List<RepositorySettings>
+            {
+                new RepositorySettings(),
+                new RepositorySettings()
+            };
+            var engine = MakeGithubEngine(0, repos);
+
+            var count = await engine.Run();
+
+            Assert.That(count, Is.EqualTo(0));
+        }
+
+        private static GithubEngine MakeGithubEngine(
+            List<RepositorySettings> repos)
+        {
+            return MakeGithubEngine(1, repos);
+        }
+
+            private static GithubEngine MakeGithubEngine(
+            int repoEngineResult,
+            List<RepositorySettings> repos)
         {
             var github = Substitute.For<IGithub>();
             var repoDiscovery = Substitute.For<IGithubRepositoryDiscovery>();
@@ -23,15 +87,21 @@ namespace NuKeeper.Tests.Engine
             github.GetCurrentUser().Returns(
                 RepositoryBuilder.MakeUser("http://test.user.com"));
 
+            repoDiscovery.GetRepositories()
+                .Returns(repos);
+
+            repoEngine.Run(
+                    Arg.Any<RepositorySettings>(),
+                    Arg.Any<UsernamePasswordCredentials>(),
+                    Arg.Any<Identity>())
+                .Returns(repoEngineResult);
+
             var setings = new GithubAuthSettings(new Uri("http://foo.com"), "token123");
 
             var engine = new GithubEngine(github, repoDiscovery, repoEngine,
                 setings,
                 folders, new NullNuKeeperLogger());
-
-            var count = await engine.Run();
-
-            Assert.That(count, Is.EqualTo(0));
+            return engine;
         }
     }
 }
