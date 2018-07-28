5527884b1db1cb95f9b4cc8f4db41f4e1070f35d
Anthony Steele <AnthonySteele@users.noreply.github.com>
2018-07-28 22:19:40 +0100

    Update octokit by hand (#357)
    Update octokit by hand
    and fix test data

--- NuKeeper.Tests/Engine/RepositoryBuilder.cs
+++ NuKeeper.Tests/Engine/RepositoryBuilder.cs
@@ -28,11 +28,16 @@ namespace NuKeeper.Tests.Engine
             var perms = new RepositoryPermissions(false, canPush, canPull);
             var parent = MakeParentRepo();
 
-            return new Repository(omniUrl, forkHtmlUrl, forkCloneUrl, omniUrl, omniUrl, omniUrl, omniUrl,
-                123, owner, "repoName", "repoName", "a test repo", omniUrl, "EN", false, true,
-                1, 1, "master", 1, null, DateTimeOffset.Now, DateTimeOffset.Now,
+            return new Repository(omniUrl, forkHtmlUrl, forkCloneUrl,
+                omniUrl, omniUrl, omniUrl, omniUrl,
+                123, "nodeId",
+                owner, "repoName", "repoName", "a test repo", "homepage",
+                "EN", false, true,
+                1, 1, "master",
+                1, null, DateTimeOffset.Now, DateTimeOffset.Now,
                 perms, parent, null, null,
-                false, false, false, false, 2, 122, true, true, true);
+                false, false, false, false,
+                2, 122, true, true, true, false);
         }
 
         private static Repository MakeParentRepo(
@@ -44,20 +49,24 @@ namespace NuKeeper.Tests.Engine
 
             var perms = new RepositoryPermissions(false, true, true);
 
-            return new Repository(omniUrl, htmlUrl, cloneUrl, omniUrl, omniUrl, omniUrl, omniUrl,
-                123, owner, "repoName", "repoName", "a test repo", omniUrl, "EN", false, true,
+            return new Repository(omniUrl, htmlUrl, cloneUrl,
+                omniUrl, omniUrl, omniUrl, omniUrl,
+                123, "nodeId",
+                owner, "repoName", "repoName", "a test repo", omniUrl, "EN", false, true,
                 1, 1, "master", 1, null,
                 DateTimeOffset.Now, DateTimeOffset.Now,
                 perms, null, null, null,
-                false, false, false, false, 2, 122, true, true, true);
+                false, false, false, false, 2, 122, true, true, true, false);
         }
 
         public static User MakeUser(string omniUrl)
         {
             return new User(omniUrl, "test user", null, 0, "test inc",
                 DateTimeOffset.Now, DateTimeOffset.Now,
-                0, null, 0, 0, false, omniUrl, 1, 1,
+                0, "testuser@test.com", 0, 0,
+                false, omniUrl, 1, 1,
                 "testville", "testUser", "Testy",
+                "nodeId",
                 1, null, 0, 0,
                 1, omniUrl, null, false, "test", null);
         }
