50cb784beaa6dcef73c3315d4c687e9f79034ff6
Anthony Steele <anthony.steele@just-eat.com>
2018-08-12 18:49:45 +0100

    Tests do something

--- NuKeeper.Tests/Local/LocalEngineTests.cs
+++ NuKeeper.Tests/Local/LocalEngineTests.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using NSubstitute;
 using NuKeeper.Configuration;
 using NuKeeper.Creators;
 using NuKeeper.Inspection;
+using NuKeeper.Inspection.Files;
 using NuKeeper.Inspection.Logging;
+using NuKeeper.Inspection.NuGetApi;
+using NuKeeper.Inspection.RepositoryInspection;
 using NuKeeper.Inspection.Sort;
 using NuKeeper.Inspection.Sources;
 using NuKeeper.Local;
@@ -17,7 +21,8 @@ namespace NuKeeper.Tests.Local
         [Test]
         public async Task CanRunInspect()
         {
-            var engine = MakeLocalEngine();
+            var updater = Substitute.For<ILocalUpdater>();
+            var engine = MakeLocalEngine(updater);
 
             var settings = new SettingsContainer
             {
@@ -29,18 +34,58 @@ namespace NuKeeper.Tests.Local
             };
 
             await engine.Run(settings);
+
+            await updater
+                .Received(0)
+                .ApplyUpdates(
+                    Arg.Any<IReadOnlyCollection<PackageUpdateSet>>(),
+                    Arg.Any<NuGetSources>());
         }
 
-        private static LocalEngine MakeLocalEngine()
+        [Test]
+        public async Task CanRunUpdate()
+        {
+            var updater = Substitute.For<ILocalUpdater>();
+            var engine = MakeLocalEngine(updater);
+
+            var settings = new SettingsContainer
+            {
+                ModalSettings = new ModalSettings
+                {
+                    Mode = RunMode.Update
+                },
+                UserSettings = new UserSettings()
+            };
+
+            await engine.Run(settings);
+
+            await updater
+                .Received(1)
+                .ApplyUpdates(
+                    Arg.Any<IReadOnlyCollection<PackageUpdateSet>>(),
+                    Arg.Any<NuGetSources>());
+        }
+
+        private static LocalEngine MakeLocalEngine(ILocalUpdater updater)
         {
             var reader = Substitute.For<INuGetSourcesReader>();
             var finder = Substitute.For<IUpdateFinder>();
+            finder.FindPackageUpdateSets(
+                    Arg.Any<IFolder>(), Arg.Any<NuGetSources>(),
+                    Arg.Any<VersionChange>())
+                .Returns(new List<PackageUpdateSet>());
+
             var sorter = Substitute.For<IPackageUpdateSetSort>();
+            sorter.Sort(Arg.Any<IReadOnlyCollection<PackageUpdateSet>>())
+                .Returns(x => x.ArgAt<IReadOnlyCollection<PackageUpdateSet>>(0));
+
             var logger = Substitute.For<INuKeeperLogger>();
 
-            var updater = Substitute.For<ICreate<ILocalUpdater>>();
+            var updaterCreator = Substitute.For<ICreate<ILocalUpdater>>();
+            updaterCreator.Create(Arg.Any<SettingsContainer>())
+                .Returns(updater);
 
-            var engine = new LocalEngine(reader, finder, sorter, updater, logger);
+            var engine = new LocalEngine(reader, finder, sorter, updaterCreator, logger);
             Assert.That(engine, Is.Not.Null);
             return engine;
         }
