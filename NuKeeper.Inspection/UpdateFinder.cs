1514ff8f4fcec2a56933cb62e26e755e0d38eff8
Anthony Steele <AnthonySteele@users.noreply.github.com>
2018-09-25 13:30:30 +0100

    log error on versioned metapackage (#443)
    * log error on versioned metapackage
    
    * Extract functions
    
    * Also log the path

--- NuKeeper.Inspection/UpdateFinder.cs
+++ NuKeeper.Inspection/UpdateFinder.cs
@@ -16,12 +16,13 @@ namespace NuKeeper.Inspection
         private readonly IPackageUpdatesLookup _packageUpdatesLookup;
         private readonly INuKeeperLogger _logger;
 
-        // ASP.NET Core has well know packages that should either be implicitly versioned, or carefully selected based on installed runtime
-        private static readonly List<string> KnownIgnoredPackages = new List<string>
-        {
-            "Microsoft.AspNetCore.App",
-            "Microsoft.AspNetCore.All"
-        };
+        // ASP.NET Core has well known metapackages that should be implicitly versioned
+        // based on installed runtime
+        private static readonly IReadOnlyCollection<string> KnownMetapackage = new List<string>
+            {
+                "Microsoft.AspNetCore.App",
+                "Microsoft.AspNetCore.All"
+            };
 
         public UpdateFinder(
             IRepositoryScanner repositoryScanner,
@@ -38,10 +39,7 @@ namespace NuKeeper.Inspection
             NuGetSources sources,
             VersionChange allowedChange)
         {
-            // scan for nuget packages
-            var packages = _repositoryScanner.FindAllNuGetPackages(workingFolder)
-                .Where(x => !KnownIgnoredPackages.Contains(x.Id, StringComparer.OrdinalIgnoreCase))
-                .ToList();
+            var packages = FindPackages(workingFolder);
 
             _logger.Log(PackagesFoundLogger.Log(packages));
 
@@ -51,6 +49,29 @@ namespace NuKeeper.Inspection
 
             _logger.Log(UpdatesLogger.Log(updates));
             return updates;
+        }
+
+        private IReadOnlyCollection<PackageInProject> FindPackages(IFolder workingFolder)
+        {
+            // scan for nuget packages
+            var allPackages = _repositoryScanner.FindAllNuGetPackages(workingFolder);
+
+            var metaPackages = allPackages
+                .Where(x => KnownMetapackage.Contains(x.Id, StringComparer.OrdinalIgnoreCase));
+
+            foreach (var metaPackage in metaPackages)
+            {
+                LogVersionedMetapackage(metaPackage);
+            }
+
+            return allPackages.Except(metaPackages)
+                .ToList();
+        }
+
+        private void LogVersionedMetapackage(PackageInProject metaPackage)
+        {
+            _logger.Error($"Metapackage '{metaPackage.Id}' has version {metaPackage.Version} in {metaPackage.Path.FullName}, " +
+                  "but should not have explicit version.");
         }
     }
 }
