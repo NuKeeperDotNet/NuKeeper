5bd70dcae459ca15adae51003c400ed66b8ebc27
Anthony Steele <anthony.steele@just-eat.com>
2018-10-08 21:14:59 +0100

    More metrics

--- NuKeeper.Inspection/Report/Formats/MetricsReportFormat.cs
+++ NuKeeper.Inspection/Report/Formats/MetricsReportFormat.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using NuGet.Versioning;
 using NuKeeper.Inspection.RepositoryInspection;
 
 namespace NuKeeper.Inspection.Report.Formats
@@ -15,13 +16,47 @@ namespace NuKeeper.Inspection.Report.Formats
 
         public void Write(string name, IReadOnlyCollection<PackageUpdateSet> updates)
         {
-            _writer.WriteLine($"PackageUpdates: {updates.Count}");
-
-            // todo: count major, minor and patch updates
-
+            _writer.WriteLine($"Package updates: {updates.Count}");
+            WriteMajorMinorPatchCount(updates);
             WriteProjectCount(updates);
             WriteLibYears(updates);
             _writer.Close();
+        }
+
+        private void WriteMajorMinorPatchCount(IReadOnlyCollection<PackageUpdateSet> updates)
+        {
+            var majors = 0;
+            var minors = 0;
+            var patches = 0;
+            foreach (var update in updates)
+            {
+                var selectedUpdate = update.SelectedVersion;
+                var minCurrent = MinCurrentVersion(update);
+
+                if (selectedUpdate.Major > minCurrent.Major)
+                {
+                    majors++;
+                }
+                else if (selectedUpdate.Minor > minCurrent.Minor)
+                {
+                    minors++;
+                }
+                else if (selectedUpdate.Patch > minCurrent.Patch)
+                {
+                    patches++;
+                }
+            }
+
+            _writer.WriteLine($"Major version updates: {majors}");
+            _writer.WriteLine($"Minor version updates: {minors}");
+            _writer.WriteLine($"Patch version updates: {patches}");
+        }
+
+        private static NuGetVersion MinCurrentVersion(PackageUpdateSet updates)
+        {
+            return updates.CurrentPackages
+                .Select(p => p.Version)
+                .Min();
         }
 
         private void WriteProjectCount(IReadOnlyCollection<PackageUpdateSet> updates)
@@ -32,7 +67,7 @@ namespace NuKeeper.Inspection.Report.Formats
                 .Distinct()
                 .Count();
 
-            _writer.WriteLine($"Projects: {projectCount}");
+            _writer.WriteLine($"Projects with updates: {projectCount}");
         }
 
         private void WriteLibYears(IReadOnlyCollection<PackageUpdateSet> updates)
