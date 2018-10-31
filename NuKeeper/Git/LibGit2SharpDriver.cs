ad6352fc429b7f93534fcddad1d135d6ed2607f7
Shaun Pearson <spearson@fiscaltec.com>
2018-10-31 17:21:13 +0000

    Move NuKeeperException to Abstractions

--- NuKeeper/Git/LibGit2SharpDriver.cs
+++ NuKeeper/Git/LibGit2SharpDriver.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using LibGit2Sharp;
+using NuKeeper.Abstractions;
 using NuKeeper.Abstractions.Logging;
 using NuKeeper.Inspection;
 using NuKeeper.Inspection.Files;
