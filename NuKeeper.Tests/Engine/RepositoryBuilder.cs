c593626093fa02b13eb67dbaffe424dc797d019c
Shaun Pearson <shep1987@gmail.com>
2018-11-02 15:18:37 +0000

    Remove Octokit from Nukeeper.Tests (#501)

--- NuKeeper.Tests/Engine/RepositoryBuilder.cs
+++ NuKeeper.Tests/Engine/RepositoryBuilder.cs
@@ -1,5 +1,5 @@
+using NuKeeper.Abstractions.DTOs;
 using System;
-using Octokit;
 
 #pragma warning disable CA1054
 
@@ -22,56 +22,38 @@ namespace NuKeeper.Tests.Engine
         public static Repository MakeRepository(
             string forkHtmlUrl = ForkHtmlUrl,
             string forkCloneUrl = ForkCloneUrl,
-            bool canPull = true, bool canPush = true,
+            bool canPull = true,
+            bool canPush = true,
             string name = "repoName")
         {
-            const string omniUrl = "http://somewhere.com/fork";
-            var owner = MakeUser(omniUrl);
-
-            var perms = new RepositoryPermissions(false, canPush, canPull);
-            var parent = MakeParentRepo();
-
-            return new Repository(omniUrl, forkHtmlUrl, forkCloneUrl,
-                omniUrl, omniUrl, omniUrl, omniUrl,
-                123, "nodeId",
-                owner, name, name, "a test repo", "homepage",
-                "EN", false, true,
-                1, 1, "master",
-                1, null, DateTimeOffset.Now, DateTimeOffset.Now,
-                perms, parent, null, null,
-                false, false, false, false,
-                2, 122, true, true, true, false);
+            return new Repository(
+                name,
+                false,
+                new UserPermissions(false, canPush, canPull),
+                new Uri(forkHtmlUrl),
+                new Uri(forkCloneUrl),
+                MakeUser(),
+                true,
+                MakeParentRepo());
         }
 
-        private static Repository MakeParentRepo(
-            string htmlUrl = ParentHtmlUrl,
-            string cloneUrl = ParentCloneUrl)
+        private static Repository MakeParentRepo()
         {
-            const string omniUrl = "http://somewhere.com/parent";
-            var owner = MakeUser(omniUrl);
-
-            var perms = new RepositoryPermissions(false, true, true);
-
-            return new Repository(omniUrl, htmlUrl, cloneUrl,
-                omniUrl, omniUrl, omniUrl, omniUrl,
-                123, "nodeId",
-                owner, "repoName", "repoName", "a test repo", omniUrl, "EN", false, true,
-                1, 1, "master", 1, null,
-                DateTimeOffset.Now, DateTimeOffset.Now,
-                perms, null, null, null,
-                false, false, false, false, 2, 122, true, true, true, false);
+            return new Repository(
+                "repoName",
+                false,
+                new UserPermissions(false, true, true),
+                new Uri(ParentCloneUrl),
+                new Uri(ParentHtmlUrl),
+                MakeUser(),
+                true,
+                null
+            );
         }
 
-        public static User MakeUser(string omniUrl)
+        public static User MakeUser()
         {
-            return new User(omniUrl, "test user", null, 0, "test inc",
-                DateTimeOffset.Now, DateTimeOffset.Now,
-                0, "testuser@test.com", 0, 0,
-                false, omniUrl, 1, 1,
-                "testville", "testUser", "Testy",
-                "nodeId",
-                1, null, 0, 0,
-                1, omniUrl, null, false, "test", null);
+            return new User("testUser", "Testy", "testuser@test.com");
         }
     }
 }
