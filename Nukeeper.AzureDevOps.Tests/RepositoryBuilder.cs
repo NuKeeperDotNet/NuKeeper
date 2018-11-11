256313ad7815add689274b7c83790d4ee187b3e4
Shaun Pearson <spearson@fiscaltec.com>
2018-11-11 17:25:33 +0000

    Add Tests to azure

---
+++ Nukeeper.AzureDevOps.Tests/RepositoryBuilder.cs
@@ -0,0 +1,61 @@
+using System;
+using NuKeeper.Abstractions.DTOs;
+
+#pragma warning disable CA1054
+
+namespace Nukeeper.AzureDevOps.Tests
+{
+    public static class RepositoryBuilder
+    {
+#pragma warning disable CA1051 // Do not declare visible instance fields
+#pragma warning disable CA2211 // Non-constant fields should not be visible
+        public static Uri ParentHtmlUrl = new Uri("http://repos.com/org/parent");
+        public static Uri ParentCloneUrl = new Uri("http://repos.com/org/parent.git");
+
+        public static Uri ForkHtmlUrl = new Uri("http://repos.com/org/repo");
+        public static Uri ForkCloneUrl = new Uri("http://repos.com/org/repo.git");
+        public static Uri NoMatchUrl = new Uri("http://repos.com/org/nomatch");
+
+        public static Repository MakeRepository(bool canPull, bool canPush)
+        {
+            return MakeRepository(ForkHtmlUrl, ForkCloneUrl, canPull, canPush);
+        }
+
+        public static Repository MakeRepository(
+            Uri forkHtmlUrl = null,
+            Uri forkCloneUrl = null,
+            bool canPull = true,
+            bool canPush = true,
+            string name = "repoName")
+        {
+            return new Repository(
+                name,
+                false,
+                new UserPermissions(false, canPush, canPull),
+                forkHtmlUrl ?? ForkHtmlUrl,
+                forkCloneUrl ?? ForkCloneUrl,
+                MakeUser(),
+                true,
+                MakeParentRepo());
+        }
+
+        private static Repository MakeParentRepo()
+        {
+            return new Repository(
+                "repoName",
+                false,
+                new UserPermissions(false, true, true),
+                ParentCloneUrl,
+                ParentHtmlUrl,
+                MakeUser(),
+                true,
+                null
+            );
+        }
+
+        public static User MakeUser()
+        {
+            return new User("testUser", "Testy", "testuser@test.com");
+        }
+    }
+}
