72b387a6cc398621990cf58d46d69e970a96a9d8
Anthony Steele <Anthony@PlasticAvalon.org>
2018-11-18 11:14:12 +0000

    Tighter hash test

--- NuKeeper.Abstractions.Tests/Formats/HasherTests.cs
+++ NuKeeper.Abstractions.Tests/Formats/HasherTests.cs
@@ -11,6 +11,7 @@ namespace NuKeeper.Abstractions.Tests.Formats
         {
             var output = Hasher.Hash("test");
 
+            AssertHashFormat(output);
             Assert.That(output, Is.EqualTo("098F6BCD4621D373CADE4E832627B4F6"));
         }
 
@@ -21,7 +22,17 @@ namespace NuKeeper.Abstractions.Tests.Formats
             var output1 = Hasher.Hash(input);
             var output2 = Hasher.Hash(input);
 
+            AssertHashFormat(output1);
+            AssertHashFormat(output2);
+
             Assert.That(output1, Is.EqualTo(output2));
+        }
+
+        private static void AssertHashFormat(string hash)
+        {
+            Assert.That(hash, Is.Not.Null);
+            Assert.That(hash, Is.Not.Empty);
+            Assert.That(hash.Length, Is.EqualTo(32));
         }
     }
 }
