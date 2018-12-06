9a086cb572a74770c40067a3f03a63f3a225a0fb
Anthony Steele <Anthony@PlasticAvalon.org>
2018-12-06 11:27:23 +0000

    go expression crazy

--- NuKeeper.Abstractions/RegexMatch.cs
+++ NuKeeper.Abstractions/RegexMatch.cs
@@ -5,20 +5,12 @@ namespace NuKeeper.Abstractions
     public static class RegexMatch
     {
         public static bool IncludeExclude(string target, Regex include, Regex exclude)
-        {
-            return
-                IsIncluded(target, include)
-                && !IsExcluded(target, exclude);
-        }
+            => IsIncluded(target, include) && !IsExcluded(target, exclude);
 
         private static bool IsIncluded(string target, Regex include)
-        {
-            return include == null || include.IsMatch(target);
-        }
+            => include?.IsMatch(target) ?? true;
 
         private static bool IsExcluded(string target, Regex exclude)
-        {
-            return exclude != null && exclude.IsMatch(target);
-        }
+            => exclude?.IsMatch(target) ?? false;
     }
 }
