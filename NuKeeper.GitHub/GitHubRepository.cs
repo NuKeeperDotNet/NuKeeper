9f5aaa3787c34698226cd600ac839a5297524ed1
Anthony Steele <anthony.steele@just-eat.com>
2019-01-19 16:34:22 +0000

    This works if the github urls are always in the same form i.e. without the ".git" suffix

--- NuKeeper.GitHub/GitHubRepository.cs
+++ NuKeeper.GitHub/GitHubRepository.cs
@@ -3,7 +3,7 @@ using NuKeeper.Abstractions.CollaborationModels;
 
 namespace NuKeeper.GitHub
 {
-    public class GitHubRepository : Abstractions.CollaborationModels.Repository
+    public class GitHubRepository : Repository
     {
         public GitHubRepository(Octokit.Repository repository)
         : base(
@@ -11,13 +11,28 @@ namespace NuKeeper.GitHub
             repository.Archived,
             repository.Permissions != null ?
                 new UserPermissions(repository.Permissions.Admin, repository.Permissions.Push, repository.Permissions.Pull) : null,
-            new Uri(repository.CloneUrl),
+            GithubUri(repository.CloneUrl),
             new User(repository.Owner.Login, repository.Owner.Name, repository.Owner.Email),
             repository.Fork,
             repository.Parent != null ?
                 new GitHubRepository(repository.Parent) : null
             )
         {
+        }
+
+        private static Uri GithubUri(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return null;
+            }
+
+            if (value.EndsWith(".git", StringComparison.OrdinalIgnoreCase))
+            {
+                value = value.Substring(0, value.Length - 4);
+            }
+
+            return new Uri(value, UriKind.Absolute);
         }
     }
 }
