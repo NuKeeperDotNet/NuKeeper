30e430a2f9cb8e9611c0efcb128bcac4f921656c
Ivan Zub <ivan.zub@mendix.com>
2019-03-28 21:06:33 +0100

    Add gitlab setting reader

---
+++ NuKeeper.Gitlab.Tests/GitlabSettingsReaderTests.cs
@@ -0,0 +1,51 @@
+using System;
+using NuKeeper.Abstractions.CollaborationPlatform;
+using NUnit.Framework;
+
+namespace NuKeeper.Gitlab.Tests
+{
+    [TestFixture]
+    public class GitlabSettingsReaderTests
+    {
+        private GitlabSettingsReader _gitlabSettingsReader;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _gitlabSettingsReader = new GitlabSettingsReader();
+        }
+
+
+        [Test]
+        public void ReturnsCorrectPlatform()
+        {
+            var platform = _gitlabSettingsReader.Platform;
+
+            Assert.AreEqual(Platform.GitLab, platform);
+        }
+
+
+        [Test]
+        public void ReturnsTrueForGitLabUrl()
+        {
+            var canRead = _gitlabSettingsReader.CanRead(new Uri("https://gitlab.com/user/projectname.git"));
+
+            Assert.AreEqual(true, canRead);
+        }
+
+        [Test]
+        public void RepositorySettingsGetsCorrectSettings()
+        {
+            var repositoryUri = new Uri("https://gitlab.com/user/projectname.git");
+            var repositorySettings = _gitlabSettingsReader.RepositorySettings(repositoryUri, "master");
+
+            Assert.IsNotNull(repositorySettings);
+            Assert.AreEqual(new Uri("https://gitlab.com/api/v4/"), repositorySettings.ApiUri);
+            Assert.AreEqual(repositoryUri, repositorySettings.RepositoryUri);
+            Assert.AreEqual("user", repositorySettings.RepositoryOwner);
+            Assert.AreEqual("projectname", repositorySettings.RepositoryName);
+            Assert.AreEqual("master", repositorySettings.TargetBranch);
+        }
+    }
+}
--- NuKeeper.Gitlab/GitlabSettingsReader.cs
+++ NuKeeper.Gitlab/GitlabSettingsReader.cs
@@ -1,26 +1,69 @@
 using System;
+using System.Linq;
+using NuKeeper.Abstractions;
 using NuKeeper.Abstractions.CollaborationPlatform;
 using NuKeeper.Abstractions.Configuration;
+using NuKeeper.Abstractions.Formats;
 
 namespace NuKeeper.Gitlab
 {
-    public class GitlabSettingsReader: ISettingsReader
+    public class GitlabSettingsReader : ISettingsReader
     {
+        private const string GitLabTokenEnvironmentVariableName = "NuKeeper_gitlab_token";
+        private const string UrlPattern = "https://gitlab.com/{username}/{projectname}.git";
+
         public Platform Platform => Platform.GitLab;
-        
+
         public bool CanRead(Uri repositoryUri)
         {
-            throw new NotImplementedException();
-        }
-
-        public RepositorySettings RepositorySettings(Uri repositoryUri, string targetBranch = null)
-        {
-            throw new NotImplementedException();
+            return repositoryUri?.Host.Contains("gitlab", StringComparison.OrdinalIgnoreCase) == true;
         }
 
         public void UpdateCollaborationPlatformSettings(CollaborationPlatformSettings settings)
         {
-            throw new NotImplementedException();
+            UpdateTokenSettings(settings);
+            settings.ForkMode = settings.ForkMode ?? ForkMode.PreferFork;
+        }
+
+        private static void UpdateTokenSettings(CollaborationPlatformSettings settings)
+        {
+            var envToken = Environment.GetEnvironmentVariable(GitLabTokenEnvironmentVariableName);
+            settings.Token = Concat.FirstValue(envToken, settings.Token);
+        }
+
+        public RepositorySettings RepositorySettings(Uri repositoryUri, string targetBranch = null)
+        {
+            if (repositoryUri == null)
+            {
+                throw new NuKeeperException(
+                    $"The provided uri was is not in the correct format. Provided null and format should be {UrlPattern}");
+            }
+
+            // Assumption - url should look https://gitlab.com/{username}/{projectname}.git";
+            var path = repositoryUri.AbsolutePath;
+            var pathParts = path.Split('/')
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .ToList();
+
+            if (pathParts.Count != 2)
+            {
+                throw new NuKeeperException(
+                    $"The provided uri was is not in the correct format. Provided {repositoryUri.ToString()} and format should be {UrlPattern}");
+            }
+
+            var repoOwner = pathParts[0];
+            var repoName = pathParts[1].Replace(".git", string.Empty);
+
+            var uriBuilder = new UriBuilder(repositoryUri) {Path = "/api/v4/"};
+
+            return new RepositorySettings
+            {
+                ApiUri = uriBuilder.Uri,
+                RepositoryUri = repositoryUri,
+                RepositoryName = repoName,
+                RepositoryOwner = repoOwner,
+                TargetBranch = targetBranch
+            };
         }
     }
 }
