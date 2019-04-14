c50927fd264dffb87e53751effd2b53d73a2af57
Stefan Kneidl <stefan.kneidl@alpine.de>
2019-04-14 14:45:16 +0200

    implement gitea forkfinder

--- NuKeeper.Gitea/GiteaForkFinder.cs
+++ NuKeeper.Gitea/GiteaForkFinder.cs
@@ -15,11 +15,6 @@ namespace NuKeeper.Gitea
 
         public GiteaForkFinder(ICollaborationPlatform collaborationPlatform, INuKeeperLogger logger, ForkMode forkMode)
         {
-            //if (forkMode != ForkMode.SingleRepositoryOnly)
-            //{
-            //    throw new ArgumentOutOfRangeException(nameof(forkMode), $"{_forkMode} has not yet been implemented");
-            //}
-
             _collaborationPlatform = collaborationPlatform;
             _logger = logger;
             _forkMode = forkMode;
@@ -29,7 +24,61 @@ namespace NuKeeper.Gitea
 
         public async Task<ForkData> FindPushFork(string userName, ForkData fallbackFork)
         {
-            return await FindUpstreamRepoOnly(fallbackFork);
+            switch (_forkMode)
+            {
+                case ForkMode.PreferFork:
+                    return await FindUserForkOrUpstream(userName, fallbackFork);
+
+                case ForkMode.PreferSingleRepository:
+                    return await FindUpstreamRepoOrUserFork(userName, fallbackFork);
+
+                case ForkMode.SingleRepositoryOnly:
+                    return await FindUpstreamRepoOnly(fallbackFork);
+
+                default:
+                    throw new ArgumentOutOfRangeException($"Unknown fork mode: {_forkMode}");
+            }
+        }
+
+        private async Task<ForkData> FindUserForkOrUpstream(string userName, ForkData pullFork)
+        {
+            var userFork = await TryFindUserFork(userName, pullFork);
+            if (userFork != null)
+            {
+                return userFork;
+            }
+
+            // as a fallback, we want to pull and push from the same origin repo.
+            var canUseOriginRepo = await IsPushableRepo(pullFork);
+            if (canUseOriginRepo)
+            {
+                _logger.Normal($"No fork for user {userName}. Using upstream fork for user {pullFork.Owner} at {pullFork.Uri}");
+                return pullFork;
+            }
+
+            NoPushableForkFound(pullFork.Name);
+            return null;
+        }
+
+        private async Task<ForkData> FindUpstreamRepoOrUserFork(string userName, ForkData pullFork)
+        {
+            // prefer to pull and push from the same origin repo.
+            var canUseOriginRepo = await IsPushableRepo(pullFork);
+            if (canUseOriginRepo)
+            {
+                _logger.Normal($"Using upstream fork as push, for user {pullFork.Owner} at {pullFork.Uri}");
+                return pullFork;
+            }
+
+            // fall back to trying a fork
+            var userFork = await TryFindUserFork(userName, pullFork);
+            if (userFork != null)
+            {
+                return userFork;
+            }
+
+            NoPushableForkFound(pullFork.Name);
+            return null;
         }
 
         private async Task<ForkData> FindUpstreamRepoOnly(ForkData pullFork)
@@ -55,6 +104,63 @@ namespace NuKeeper.Gitea
         {
             var originRepo = await _collaborationPlatform.GetUserRepository(originFork.Owner, originFork.Name);
             return originRepo?.UserPermissions.Push == true;
+        }
+
+        private async Task<ForkData> TryFindUserFork(string userName, ForkData originFork)
+        {
+            var userFork = await _collaborationPlatform.GetUserRepository(userName, originFork.Name);
+            if (userFork != null)
+            {
+                var isMatchingFork = RepoIsForkOf(userFork, originFork.Uri);
+                var forkIsPushable = userFork.UserPermissions.Push;
+                if (isMatchingFork && forkIsPushable)
+                {
+                    // the user has a pushable fork
+                    return RepositoryToForkData(userFork);
+                }
+
+                // the user has a repo of that name, but it can't be used. 
+                // Don't try to create it
+                _logger.Normal($"User '{userName}' fork of '{originFork.Name}' exists but is unsuitable. Matching: {isMatchingFork}. Pushable: {forkIsPushable}");
+                return null;
+            }
+
+            // no user fork exists, try and create it as a fork of the main repo
+            var newFork = await _collaborationPlatform.MakeUserFork(originFork.Owner, originFork.Name);
+            if (newFork != null)
+            {
+                return RepositoryToForkData(newFork);
+            }
+
+            return null;
+        }
+
+        private static bool RepoIsForkOf(Repository userRepo, Uri originRepo)
+        {
+            if (!userRepo.Fork)
+            {
+                return false;
+            }
+
+            if (userRepo.Parent?.CloneUrl == null)
+            {
+                return false;
+            }
+
+            if (originRepo == null)
+            {
+                return false;
+            }
+
+            var userParentUrl = userRepo.Parent.CloneUrl;
+            var originUrl = originRepo;
+
+            return userParentUrl.Equals(originUrl);
+        }
+
+        private static ForkData RepositoryToForkData(Repository repo)
+        {
+            return new ForkData(repo.CloneUrl, repo.Owner.Login, repo.Name);
         }
     }
 }
