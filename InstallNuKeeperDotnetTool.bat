aecb8b2af675f310f43634928c34839f35ce167e
Jisper <52332718+jisperliu@users.noreply.github.com>
2019-08-06 17:14:00 -0400

    Update InstallNuKeeperDotnetTool.bat
    pack artifacts to root directory instead of NuKeeper/artifacts

--- InstallNuKeeperDotnetTool.bat
+++ InstallNuKeeperDotnetTool.bat
@@ -5,7 +5,7 @@ echo Build solution:
 dotnet build 
 
 echo Pack NuKeeper dotnet tool:
-dotnet pack .\NuKeeper\NuKeeper.csproj -o ".\artifacts"
+dotnet pack .\NuKeeper\NuKeeper.csproj -o "..\artifacts"
 
 echo Uninstall existing NuKeeper dotnet tool:
 dotnet tool uninstall nukeeper --global
