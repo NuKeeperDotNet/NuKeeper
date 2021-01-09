5900d9dd7f762f0edd4b95ee0ecc44d9c69b2a2b
krissetto <chrisjpetito@gmail.com>
2021-01-09 22:04:42 +0100

    Fixes to correctly check if a repo contains dotnet code

--- NuKeeper.Abstractions/CollaborationModels/SearchCodeRequest.cs
+++ NuKeeper.Abstractions/CollaborationModels/SearchCodeRequest.cs
@@ -17,6 +17,12 @@ namespace NuKeeper.Abstractions.CollaborationModels
 
     public class SearchCodeRequest
     {
+        public SearchCodeRequest(IEnumerable<string> extensions, IEnumerable<SearchRepo> repos)
+        {
+            Extensions = extensions;
+            Repos = repos.ToList();
+        }
+
         public SearchCodeRequest(string term, IEnumerable<SearchRepo> repos)
         {
             Term = term;
@@ -24,6 +30,7 @@ namespace NuKeeper.Abstractions.CollaborationModels
         }
 
         public string Term { get; }
+        public IEnumerable<string> Extensions { get; }
         public IReadOnlyCollection<SearchRepo> Repos { get; }
         public int PerPage { get; set; }
     }
