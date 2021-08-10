cb75ad0ffb62bb3d4c5c8e75023a2b39e7c925b6
Jamie Clayton <jamie@jenasys.com>
2021-08-10 21:55:45 +1000

    Fix unit tests to allow for multiple operating systems
    * Ensure that non windows OS's execute unit tests.

--- NuKeeper.Tests/Engine/NugetRestoreTests.cs
+++ NuKeeper.Tests/Engine/NugetRestoreTests.cs
@@ -5,6 +5,7 @@ using NuKeeper.Update.Process;
 using NuKeeper.Update.ProcessRunner;
 using NUnit.Framework;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace NuKeeper.Tests.Engine
@@ -16,17 +17,34 @@ namespace NuKeeper.Tests.Engine
         public async Task WhenNugetRestoreIsCalledThenArgsIncludePackageDirectory()
         {
             var logger = Substitute.For<INuKeeperLogger>();
-            var nuGetPath = Substitute.For<INuGetPath>();           
+            var nuGetPath = Substitute.For<INuGetPath>();
             var monoExecuter = Substitute.For<IMonoExecutor>();
             var externalProcess = Substitute.For<IExternalProcess>();
-            var file = new FileInfo("packages.config");           
+            var file = new FileInfo("packages.config");
             nuGetPath.Executable.Returns(@"c:\DoesNotExist\nuget.exe");
-            externalProcess.Run(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<bool>()).Returns(new ProcessOutput("","",0));
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                externalProcess.Run(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<bool>()).Returns(new ProcessOutput("", "", 0));
+            }
+            else
+            {
+                monoExecuter.Run(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<bool>()).Returns(new ProcessOutput("", "", 0));
+            }
             var cmd = new NuGetFileRestoreCommand(logger, nuGetPath, monoExecuter, externalProcess);
+
             await cmd.Invoke(file, NuGetSources.GlobalFeed);
 
-            await externalProcess.Received(1).Run(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<bool>());
-            await externalProcess.ReceivedWithAnyArgs().Run(Arg.Any<string>(), Arg.Any<string>(), $"restore {file.Name} - Source ${NuGetSources.GlobalFeed} -NonInteractive -PackagesDirectory ..\\packages", Arg.Any<bool>());
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                await externalProcess.Received(1).Run(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<bool>());
+                await externalProcess.ReceivedWithAnyArgs().Run(Arg.Any<string>(), Arg.Any<string>(), $"restore {file.Name} - Source ${NuGetSources.GlobalFeed} -NonInteractive -PackagesDirectory ..\\packages", Arg.Any<bool>());
+            }
+            else
+            {
+                await monoExecuter.Received(1).Run(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<bool>());
+                await monoExecuter.ReceivedWithAnyArgs().Run(Arg.Any<string>(), Arg.Any<string>(), $"restore {file.Name} - Source ${NuGetSources.GlobalFeed} -NonInteractive -PackagesDirectory ..\\packages", Arg.Any<bool>());
+            }
+
         }
     }
 }
